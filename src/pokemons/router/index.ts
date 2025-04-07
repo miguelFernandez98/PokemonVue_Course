95e3598f3e738b0e548e94d231eb0bc2dbfd7e7a
miguelFernandez98 <139988212+miguelFernandez98@users.noreply.github.com>
2025-04-07 13:44:56 -0400

    `Added new routes for pokemon by id, list and search`

--- src/pokemons/router/index.ts
+++ src/pokemons/router/index.ts
@@ -8,8 +8,16 @@ export const pokemonRoute: RouteRecordRaw = {
   component: () => import('@/pokemons/layouts/PokemonLayout.vue'),
   children: [
     {
+      path: `/${ROUTE_NAME}/by/:id`,
+      component: () => import('@/pokemons/pages/pokemonById.vue')
+    },
+    {
       path: `/${ROUTE_NAME}/list`,
       component: () => import('@/pokemons/pages/pokemonList.vue')
+    },
+    {
+      path: `/${ROUTE_NAME}/search`,
+      component: () => import('@/pokemons/pages/pokemonSearch.vue')
     }
   ]
 }
